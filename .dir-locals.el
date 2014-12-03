a5b9deff64c9d6e91cb2e05aa7c84b5422aba46b
David (Pololu) <dev-david@pololu.com>
2014-12-03 10:38:16 -0800

    Added emacs settings file.

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+; Settings for the Emacs text editor.
+
+(
+  (c-mode . ((c-basic-offset . 4) (tab-width . 4) (indent-tabs-mode . nil)))
+  (nil . ((fill-column . 80)))
+  ("FastGPIO/examples" . (
+    (nil . ((c-basic-offset . 2) (tab-width . 8) (fill-column . 65)))
+  ))
+)
